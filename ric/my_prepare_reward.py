0ae2e7409c6d2282f8911dad580137faad1c8cc7
TingchenFu <1607228797@qq.com>
2024-05-22 01:03:40 +0800

    revise online generation

--- ric/my_prepare_reward.py
+++ ric/my_prepare_reward.py
@@ -4,21 +4,19 @@ This script is used to prepare the training dataset for RiC.
 from dataclasses import dataclass, field
 from typing import Optional
 import datasets
-from datasets import load_dataset, concatenate_datasets, load_from_disk
+from datasets import load_dataset
 from accelerate import Accelerator
 import numpy as np
 import pandas as pd
 import torch
 from tqdm import tqdm
 from transformers import HfArgumentParser
-from utils import Instructions_n, load_main_tokenizer, Instructions_summary_n
 from my_reward_model import RewardModel
 import os
 import json
 # define paths for two datasets
 # hhrlhf_dataset_path = 'Anthropic/hh-rlhf'
 # summary_dataset_path = 'openai/summarize_from_feedback'
-tokenizer_name = '/home/futingchen/PLM/Llama-2-7b-hf'
 
 
 def template_function_hh(sample,chosen=True):
@@ -33,40 +31,47 @@ def template_function_hh(sample,chosen=True):
     sample['completion'] = split_text[-1].strip()
     return sample
 
+
+
+def template_function_beaver(sample,criteria='better_response_id'):
+    assert criteria in ['better_response_id', 'safer_response_id']
+    chosen_id = int(sample[criteria])
+    sample['completion'] = sample['response_{}'.format(chosen_id)]    
+
+    return sample 
+
+
 @dataclass
 class ScriptArguments:
-    reward_names:Optional[str] = field(default='helpful,harmless,humor') 
-    save_directory: Optional[str] = field(default='./data/HH/')
-    exp_type: Optional[str] = field(default='assistant', metadata={"help": "exp type, 'summary' or 'assistant' "})
+    reward_tokenizer_name_or_path: Optional[str] = field(default=None)
+    reward_model_name_or_path: Optional[str] = field(default=None)
+    input_file: Optional[str] = field(default=None, metadata={"help": "The input file to score and reward"})
+    output_file: Optional[str] = field(default=None, metadata={"help": "The output file to save the rewards"})
+    
+    #reward_names:Optional[str] = field(default='helpfulharmless,humor') 
+
+
+    def __post_init__(self):
+        self.reward_model_name_or_path = self.reward_model_name_or_path.split(',')
+        if self.reward_tokenizer_name_or_path is None:
+            self.reward_tokenizer_name_or_path = self.reward_model_name_or_path
+        else:
+            self.reward_tokenizer_name_or_path = self.reward_tokenizer_name_or_path.split(',')
+            assert len(self.reward_tokenizer_name_or_path) == len(self.reward_model_name_or_path), "The number of reward model paths and tokenizer paths should be the same."
+        self.output_file = self.output_file.split(',')
+        assert len(self.output_file) == len(self.reward_model_name_or_path)
+    
+
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
-reward_names = [x.strip() for x in script_args.reward_names.split(',')]
-print(reward_names)
-reward_path_tokenizer_dict = {
-    'harmless': ['/home/futingchen/PLM/gpt2large_harmless_reward'],
-    'helpful': ['/home/futingchen/PLM/gpt2large_helpful_reward'],
-    'deberta': ['OpenAssistant/reward-model-deberta-v3-large-v2'],
-    'summary': ['Tristan/gpt2_reward_summarization'],
-    'faithful':['CogComp/bart-faithful-summary-detector'],
-    'humor': ['/home/futingchen/PLM/distilbert_humor_reward'],
-}
-reward_model_path_list = []
-rm_tokenizer_path_list = []
-for name in reward_names:
-    if name not in reward_path_tokenizer_dict.keys():
-        raise NotImplementedError
-    reward_model_path_list.append(reward_path_tokenizer_dict[name][0])
-    rm_tokenizer_path_list.append(reward_path_tokenizer_dict[name][0])
-    
 
-tokenizer = load_main_tokenizer(tokenizer_name)
 gpu_id = Accelerator().local_process_index 
 print("GPU ID: ", gpu_id)
-reward_models = RewardModel(reward_model_path_list, rm_tokenizer_path_list, gpu_id)
-rm_tokenizers = reward_models.rm_tokenizers
+reward_models = RewardModel(script_args.reward_model_name_or_path, script_args.reward_tokenizer_name_or_path, gpu_id)
 
-raw_dataset = load_dataset('json', data_files=['/home/futingchen/MultiContrast/RiC/ric/dump/online1_Llama-2-7b-hf_HH_lora_bf16_bs4lr1e-5decay0.01constant_04111305_empty.jsonl'])
+
+raw_dataset = load_dataset('json', data_files = script_args.input_file.split(','))
 # ATTENTION
 raw_dataset = raw_dataset['train']
 #raw_dataset = raw_dataset.select(range(100))
@@ -74,7 +79,7 @@ raw_dataset = raw_dataset['train']
 #print("here is OK")
 print(len(raw_dataset))
 templated_dataset = raw_dataset.map(
-    template_function_hh,
+    template_function_hh if 'HH' in script_args.input_file else template_function_beaver,
     batched=False,
     remove_columns= None,
 )
@@ -86,5 +91,5 @@ for i in range(len(templated_dataset)):
     query_response.append((templated_dataset[i]['prompt'], templated_dataset[i]['completion']))
 reward_list = reward_models.get_multiple_reward_parallel(query_response)
 
-fout = open(os.path.join(script_args.save_directory, 'online2_reward_{}.json'.format(reward_names[gpu_id])), 'w')
+fout = open(script_args.output_file[gpu_id], 'w')
 json.dump(reward_list, fout)
--- ric/my_utils.py
+++ ric/my_utils.py
@@ -15,26 +15,41 @@ def template_function_hh(sample,chosen=True):
 
 
 
-def normalize():
-    raw_dataset = load_dataset('json', data_files=['/home/futingchen/MultiContrast/data/HH/harmless_base_train.jsonl','/home/futingchen/MultiContrast/data/HH/helpful_base_train.jsonl','/home/futingchen/MultiContrast/data/HH/helpful_online_train.jsonl', '/home/futingchen/MultiContrast/data/HH/helpful_sampled_train.jsonl'],split='train')
+def get_offline_train_HH():
+    raw_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/data/HH/harmless_base_train.jsonl','/home/tingchen_fu/MultiContrast/data/HH/helpful_base_train.jsonl','/home/tingchen_fu/MultiContrast/data/HH/helpful_online_train.jsonl', '/home/tingchen_fu/MultiContrast/data/HH/helpful_sampled_train.jsonl'],split='train')
 
-    norm_helpful_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/norm_reward_harmless.json'))
-    norm_harmless_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/norm_reward_helpful.json'))
-    norm_honor_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/norm_reward_humor.json'))
+    offline_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/norm_reward_harmless.json'))
+    offline_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/norm_reward_helpful.json'))
+    offline_honor_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/norm_reward_humor.json'))
 
-    raw_dataset = raw_dataset.add_column('helpful_reward', norm_helpful_reward)
-    raw_dataset = raw_dataset.add_column('harmless_reward', norm_harmless_reward)
-    raw_dataset = raw_dataset.add_column('humor_reward', norm_honor_reward)
+    raw_dataset = raw_dataset.add_column('helpful_reward', offline_helpful_reward)
+    raw_dataset = raw_dataset.add_column('harmless_reward', offline_harmless_reward)
+    raw_dataset = raw_dataset.add_column('humor_reward', offline_honor_reward)
 
     print(raw_dataset.column_names)
     print(raw_dataset[0])
 
-    raw_dataset.to_json('/home/futingchen/MultiContrast/RiC/ric/data/HH/train.jsonl', orient='records')
+    raw_dataset.to_json('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/train.jsonl', orient='records')
+
+
+def get_offline_train_beaver():
+    raw_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/data/beaver_train.jsonl'],split='train')
+
+    offline_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/beaver/offline_reward_helpful.json'))
+    offline_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/beaver/offline_reward_harmless.json'))
+
+    raw_dataset = raw_dataset.add_column('helpful_reward', offline_helpful_reward)
+    raw_dataset = raw_dataset.add_column('harmless_reward', offline_harmless_reward)
+
+    print(raw_dataset.column_names)
+    print(raw_dataset[0])
+
+    raw_dataset.to_json('/home/tingchen_fu/MultiContrast/RiC/ric/data/beaver/offline_train.jsonl', orient='records')
 
 
 
 def get_online1_train():
-    offline_dataset = load_dataset('json', data_files=['/home/futingchen/MultiContrast/RiC/ric/data/HH/offline_train.jsonl'],split='train')
+    offline_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/offline_train.jsonl'],split='train')
 
 
     thresholds = [np.quantile(offline_dataset['helpful_reward'], 0.7),
@@ -47,10 +62,10 @@ def get_online1_train():
     offline_dataset = offline_dataset.select(range(10000))
                                             
 
-    online1_dataset = load_dataset('json', data_files=['/home/futingchen/MultiContrast/RiC/ric/dump/offline_Llama-2-7b-hf_HH_lora_bf16_bs4lr1.41e-4decay0.01linear_04102114_empty.jsonl'],split='train')
-    online1_helpful_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/online1_reward_helpful.json'))
-    online1_harmless_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/online1_reward_harmless.json'))
-    online1_humor_reward = json.load(open('/home/futingchen/MultiContrast/RiC/ric/data/HH/online1_reward_humor.json'))
+    online1_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/dump/offline_phi-2_HH_lora_bf16_bs4lr1.41e-4decay0.01linear_04112314_empty.jsonl'],split='train')
+    online1_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_helpful.json'))
+    online1_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_harmless.json'))
+    online1_humor_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_humor.json'))
 
     online1_dataset = online1_dataset.add_column('helpful_reward', online1_helpful_reward)
     online1_dataset = online1_dataset.add_column('harmless_reward', online1_harmless_reward)
@@ -69,7 +84,107 @@ def get_online1_train():
     print(online1_dataset.column_names)
 
 
-    concatenate_datasets([offline_dataset,online1_dataset]).to_json('/home/futingchen/MultiContrast/RiC/ric/data/HH/online1_train.jsonl', orient='records')
+    concatenate_datasets([offline_dataset,online1_dataset]).to_json('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_train.jsonl', orient='records')
 
 
-get_online1_train()
+
+
+
+
+
+def get_online1_train_beaver():
+    offline_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/data/beaver_train.jsonl'],split='train')
+
+
+    thresholds = [np.quantile(offline_dataset['helpful_reward'], 0.7),
+                np.quantile(offline_dataset['harmless_reward'], 0.7),
+                np.quantile(offline_dataset['humor_reward'], 0.7)]
+    offline_dataset = offline_dataset.filter(lambda x: int(x['helpful_reward'] >= thresholds[0]) + int(x['harmless_reward'] >= thresholds[1]) + int(x['humor_reward'] >= thresholds[2])>=2)
+
+
+    offline_dataset = offline_dataset.map(template_function_hh, batched=False, remove_columns=['chosen','rejected'])
+    offline_dataset = offline_dataset.select(range(10000))
+                                            
+
+    online1_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/dump/offline_phi-2_HH_lora_bf16_bs4lr1.41e-4decay0.01linear_04112314_empty.jsonl'],split='train')
+    online1_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_helpful.json'))
+    online1_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_harmless.json'))
+    online1_humor_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_humor.json'))
+
+    online1_dataset = online1_dataset.add_column('helpful_reward', online1_helpful_reward)
+    online1_dataset = online1_dataset.add_column('harmless_reward', online1_harmless_reward)
+    online1_dataset = online1_dataset.add_column('humor_reward', online1_humor_reward)
+
+
+    thresholds = [np.quantile(online1_dataset['helpful_reward'], 0.7),
+                np.quantile(online1_dataset['harmless_reward'], 0.7),
+                np.quantile(online1_dataset['humor_reward'], 0.7)]
+    online1_dataset = online1_dataset.filter(lambda x: int(x['helpful_reward'] >= thresholds[0]) + int(x['harmless_reward'] >= thresholds[1]) + int(x['humor_reward'] >= thresholds[2])>=2)
+
+    print(len(offline_dataset))
+    print(len(online1_dataset))
+
+    print(offline_dataset.column_names)
+    print(online1_dataset.column_names)
+
+
+    concatenate_datasets([offline_dataset,online1_dataset]).to_json('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_train.jsonl', orient='records')
+
+
+
+def get_online2_train():
+    offline_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/offline_train.jsonl'],split='train')
+
+
+    thresholds = [np.quantile(offline_dataset['helpful_reward'], 0.7),
+                np.quantile(offline_dataset['harmless_reward'], 0.7),
+                np.quantile(offline_dataset['humor_reward'], 0.7)]
+    offline_dataset = offline_dataset.filter(lambda x: int(x['helpful_reward'] >= thresholds[0]) + int(x['harmless_reward'] >= thresholds[1]) + int(x['humor_reward'] >= thresholds[2])>=2)
+
+
+    offline_dataset = offline_dataset.map(template_function_hh, batched=False, remove_columns=['chosen','rejected'])
+    offline_dataset = offline_dataset.select(range(10000))
+                                            
+
+    online1_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/dump/offline_phi-2_HH_lora_bf16_bs4lr1.41e-4decay0.01linear_04112314_empty.jsonl'],split='train')
+    online1_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_helpful.json'))
+    online1_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_harmless.json'))
+    online1_humor_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online1_reward_humor.json'))
+
+    online1_dataset = online1_dataset.add_column('helpful_reward', online1_helpful_reward)
+    online1_dataset = online1_dataset.add_column('harmless_reward', online1_harmless_reward)
+    online1_dataset = online1_dataset.add_column('humor_reward', online1_humor_reward)
+
+
+    thresholds = [np.quantile(online1_dataset['helpful_reward'], 0.7),
+                np.quantile(online1_dataset['harmless_reward'], 0.7),
+                np.quantile(online1_dataset['humor_reward'], 0.7)]
+    online1_dataset = online1_dataset.filter(lambda x: int(x['helpful_reward'] >= thresholds[0]) + int(x['harmless_reward'] >= thresholds[1]) + int(x['humor_reward'] >= thresholds[2])>=2)
+
+
+    online2_dataset = load_dataset('json', data_files=['/home/tingchen_fu/MultiContrast/RiC/ric/dump/online1_Llama-2-7b-hf_HH_lora_bf16_bs4lr1e-5decay0.01constant_04121428_empty.jsonl'],split='train')
+    online2_helpful_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online2_reward_helpful.json'))
+    online2_harmless_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online2_reward_harmless.json'))
+    online2_humor_reward = json.load(open('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online2_reward_humor.json'))
+
+    online2_dataset = online2_dataset.add_column('helpful_reward', online2_helpful_reward)
+    online2_dataset = online2_dataset.add_column('harmless_reward', online2_harmless_reward)
+    online2_dataset = online2_dataset.add_column('humor_reward', online2_humor_reward)
+
+
+    thresholds = [np.quantile(online2_dataset['helpful_reward'], 0.7),
+                np.quantile(online2_dataset['harmless_reward'], 0.7),
+                np.quantile(online2_dataset['humor_reward'], 0.7)]
+    online2_dataset = online2_dataset.filter(lambda x: int(x['helpful_reward'] >= thresholds[0]) + int(x['harmless_reward'] >= thresholds[1]) + int(x['humor_reward'] >= thresholds[2])>=2)
+
+    print(len(offline_dataset))
+    print(len(online1_dataset))
+
+    print(offline_dataset.column_names)
+    print(online1_dataset.column_names)
+
+    concatenate_datasets([offline_dataset,online1_dataset]).to_json('/home/tingchen_fu/MultiContrast/RiC/ric/data/HH/online2_train.jsonl', orient='records')
+
+
+
+get_offline_train_beaver()
